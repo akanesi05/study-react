02020293cb9c1efa4fce94bb1d3fff809c2af34f
mitsuiakane <akane.sumitomo@gmail.com>
2024-12-01 21:29:12 +0900

    #7

--- components/Links.js
+++ components/Links.js
@@ -3,35 +3,33 @@ import styles from "./Links.module.css";
 import Image from "next/image"
 
 
+const ITEMS =[
+  {  href:"https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title:" Deploy now2"
+  }
+,{  href:"https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title:"Read our docs2"
+}
+]
 
 
 
 export  function Links() {
   return (
           <div className={styles.ctas}>
-            <a
+            {ITEMS.map(item=>{
+              return(
+                <a  key={item.href}href={item.href}
               className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              //href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
+              {item.title}
             </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
+
+              )
+            })}
           </div>
   );
 }
